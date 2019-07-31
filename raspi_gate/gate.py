02f6439b196171b6b7fa0bdecb5005ad60b90b64
baldarn <lorenzo.farnararo@gmail.com>
2019-07-31 09:15:40 +0200

    added color and open time state

--- raspi_gate/gate.py
+++ raspi_gate/gate.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -8,14 +8,23 @@ from raspi_gate.db import get_db
 
 from gpiozero import LED
 from time import sleep
+import datetime
 
 bp = Blueprint('gate', __name__)
 
+LAST_OPEN_KEY = 'LAST_OPEN'
+GATE_OPEN_SECONDS = 50
 
 @bp.route('/')
 def index():
     """index page."""
-    return render_template('gate/index.html')
+    print(session.get(LAST_OPEN_KEY))
+    last_open = session.get(LAST_OPEN_KEY)
+    is_open = False
+    if last_open is not None:
+        is_open = (datetime.datetime.now() - datetime.timedelta(seconds=GATE_OPEN_SECONDS)) < last_open
+
+    return render_template('gate/index.html', is_open=is_open)
 
 
 @bp.route('/open')
@@ -27,6 +36,8 @@ def open():
     led.off()
     sleep(1)
     led.on()
+
+    session[LAST_OPEN_KEY] = datetime.datetime.now()
 
     return redirect(url_for('gate.index'))
 
